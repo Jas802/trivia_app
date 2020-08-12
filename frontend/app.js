082213f904d0fbb3d56b691eeb582e3cb9450f19
Jas802 <jas.spencer414@gmail.com>
2020-08-12 10:15:53 -0400

    moved frontend file

--- frontend/app.js
+++
@@ -1,8 +0,0 @@
-// fetch('http://localhost:3000/api/v1/').then((response) => {
-//     console.log('resolved', response);
-//     return response.json();
-// }).then(data => {
-//     console.log(data);
-// }).catch((err) => {
-//     console.log('rejected', err);
-// });
